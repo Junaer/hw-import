4bc80f579580d8d246dfd7da38915feb29d9dc48
Мызников Евгений <junaers1996@gmail.com>
2022-04-07 21:48:38 +0300

    second try

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from salary import calculate_salary
-from people import get_employees
+from application.salary import calculate_salary
+from application.db.people import get_employees
 print ('main start')
 
 if __name__ == '__main__':
